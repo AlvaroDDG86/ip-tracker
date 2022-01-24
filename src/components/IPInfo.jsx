a915ce5f12742707dc707d74991675b21688422d
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-24 18:44:22 +0100

    Some responsible rules

--- src/components/IPInfo.jsx
+++ src/components/IPInfo.jsx
@@ -1,5 +1,5 @@
 export default function IPInfo(props) {
-  const { info } = props
+  const { info } = props;
   const infoLocates = [
     { title: "IP Adress", value: info.ip },
     { title: "Isp", value: info.isp },
@@ -7,12 +7,12 @@ export default function IPInfo(props) {
     { title: "Time zone", value: info.location.timezone },
   ];
   return (
-    <div className="absolute -bottom-20 flex gap-y-4 gap-x-12 px-8 pt-8 pb-12 rounded-md bg-white text-blue-800">
+    <div className="absolute -bottom-20 flex gap-y-4 gap-x-2 lg:gap-x-12 px-8 pt-8 pb-12 rounded-md bg-white text-blue-800 max-w-full">
       {infoLocates.map((item, index) => {
         return (
           <div className="flex flex-col" key={index}>
             <h3 className="text-xs mb-2 uppercase">{item.title}</h3>
-            <span className="text-2xl">{item.value}</span>
+            <span className="text-xs lg:text-2xl">{item.value}</span>
           </div>
         );
       })}
