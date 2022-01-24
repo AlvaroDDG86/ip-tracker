63bfaad42b4e4735c2a57a0139f803ac8fae060b
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-24 17:48:54 +0100

    Add the Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import background from './assets/images/bg.svg'
 import IPInfo from './components/IPInfo';
 import Search from './components/Search';
+import Map from './components/Map';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
     <div className="flex flex-col h-screen max-h-screen">
-      <div className="flex justify-center relative px-4 pt-8 pb-32 bg-cover bg-center"  style={{ backgroundImage: `url(${background})` }}>
+      <div className="z-20 flex justify-center relative px-4 pt-8 pb-32 bg-cover bg-center"  style={{ backgroundImage: `url(${background})` }}>
         <div className="w-full max-w-screen-sm">
           <h1 className="text-center text-white text-3xl pb-4">React IP Tracker</h1>
           <Search />
         </div>
         <IPInfo />
       </div>
+      <Map />
     </div>
   );
 }
