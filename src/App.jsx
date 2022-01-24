778e7d0f192df9dfe4ede9c26036f31853b15c3f
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-24 16:11:42 +0100

    adding sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import "./App.css";
+import classes from './App.module.scss'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="flex justify-center">
-      <h1 className="font-bold text-8xl text-green-200">
+      <h1 className={"font-bold text-8xl text-green-200 " + classes.title}>
         React and Tailwind with Vitejs!
       </h1>
     </div>
